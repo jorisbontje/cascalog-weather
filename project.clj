298ed086138f2a590a965543cda118638322ce49
Joris Bontje <jorisbontje@gmail.com>
2011-11-04 21:53:38 +0100

    bump hadoop as well

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [cascalog/cascalog "1.8.3"]
                  [ch.qos.logback/logback-classic "0.9.30"]]
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u0"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u2"]]
   :main cascalog-weather.weather
   :repositories {"cloudera-releases" "https://repository.cloudera.com/content/repositories/releases/"})
