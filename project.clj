f5bb987f7e418f88fb0adfcd563221c8a79976dc
Joris Bontje <jorisbontje@gmail.com>
2011-11-04 21:42:37 +0100

    bump to cascalog 1.8.3

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject cascalog-weather "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :jvm-opts ["-Xmx768m"]
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [cascalog/cascalog "1.7.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cascalog/cascalog "1.8.3"]
+                 [ch.qos.logback/logback-classic "0.9.30"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u0"]]
   :main cascalog-weather.weather
   :repositories {"cloudera-releases" "https://repository.cloudera.com/content/repositories/releases/"})
